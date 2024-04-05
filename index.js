9cbf39ffdbdab6ab67f35f5b98e99d145a5b647c
Mark Anthony Llego <markllego@gmail.com>
2024-04-06 05:20:04 +0800

    Add trust proxy setting to index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const Bottleneck = require('bottleneck');
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.set('trust proxy', true);
+
 app.get('/', (_req, res) => {
   res.send('Neko Discord Bot is running!');
 });
